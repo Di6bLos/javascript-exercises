253e1ce33c12b78123502d47a74a4500493e0d34
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-08 17:08:31 -0700

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+"use strict"
 
+
+const palindromes = function (stringInput) {
+    const remove = /[\w]/g;
+    const stringForward = stringInput.toLowerCase().match(remove).join('');
+    const stringReverse = stringForward.split('').reverse().join('');
+    
+  if (stringForward === stringReverse) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
