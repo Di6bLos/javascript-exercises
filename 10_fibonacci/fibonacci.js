a22e9f8d89d16d8305e0d4fb96df7572049ddcda
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-10 11:45:34 -0700

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = (n) => {
+  
+    const arr = [1,1];
+     if (n > 0) {
+       for (let i = 2; i < n; i++) {
+       arr[i] = arr[i-1] + arr[i-2];
+     };} else {
+       return "OOPS"
+     }
+     
+     return arr.at(-1);
+   };
+   
 // Do not edit below this line
 module.exports = fibonacci;
